19de2d4d74b98e2daa3e8300b6c0c0d445a76f45
James Maxwell <jamesalastairmaxwell@gmail.com>
2025-03-06 15:34:51 +0000

    Update search.py

--- backend/search.py
+++ backend/search.py
@@ -556,7 +556,7 @@ def sort_by_edit_distance(token, candidates):
 
 # Example usage:
 if __name__ == '__main__':
-    load_indices()
+    # load_indices()
 
     # # Example query string.
     # query = "chicken garlic lemon"
